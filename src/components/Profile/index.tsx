bbc864f7d7a89a90c300fff073eefb898567acdc
Marlon Date-Chong <mdatechong@gmail.com>
2019-02-12 18:44:21 -0500

    add colours and react router

---
+++ src/components/Profile/index.tsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './index.scss';
+
+class Profile extends Component {
+  render() {
+    return (
+      <div className="Profile">
+        <header className="Profile-header">
+          <h1>Profile</h1>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default Profile;
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { Fragment } from "react";
+import ReactDOM from "react-dom";
+import "./index.scss";
+import App from "./components/App";
+import Profile from "./components/Profile";
+import * as serviceWorker from "./serviceWorker";
+import { Route, BrowserRouter as Router, Link } from "react-router-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Router>
+    <Fragment>
+      <Route exact path="/" component={App} />
+      <Route exact path="/profile" component={Profile} />
+    </Fragment>
+  </Router>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
